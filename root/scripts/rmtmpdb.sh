76a6ea0a79934c612948243d644ed93410c4db8d
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-01-10 07:16:25 -0600

    Update rmtmpdb.sh

--- root/scripts/rmtmpdb.sh
+++ root/scripts/rmtmpdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #set -x
 # Este script elimina las conexiones creadas en http://IP/daloradius/rep-lastconnect.php de hace cierto tiempo
-# tambien elimina las entradas en http://IP/daloradius/acct-all.php
+# tambien elimina las entradas en http://IP/daloradius/acct-all.php importante ya que de aqui se toman las sumas de tiempo de los usuarios
 SQLPASS="Passw@rd"
 export MYSQL_PWD=$SQLPASS
 mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
