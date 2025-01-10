f27444a833c192b1bbc4a2e313e1bfe469bcdf37
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-01-09 21:02:56 -0600

    Update NAS-Reboot.sh

--- root/scripts/NAS-Reboot.sh
+++ root/scripts/NAS-Reboot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #set -x
-# This script delets users who have expired 2 months ago. and then delete there records from all tables.
+# Elimina los errores que causan por desconexion o perdida en mikrotik, a daloradius en accounting llamados NAS-Reboot
+# Este script en el crontab -e se agrega:
 # */5  * * * * sudo bash /root/scripts/NAS-Reboot.sh
 SQLPASS="Passw@rd"
 export MYSQL_PWD=$SQLPASS
 
 mysql -uroot -e "use radius; DELETE FROM radacct WHERE acctterminatecause ='NAS-Reboot';" 
-
 
 echo "Limpiado los NAS-Reboot"
