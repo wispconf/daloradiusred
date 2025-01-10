d8b045db08235df481765cd8e3118200bc8b9b3a
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-01-09 20:49:35 -0600

    Update eliminabatch.sh

--- root/scripts/cleaner/eliminabatch.sh
+++ root/scripts/cleaner/eliminabatch.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #set -x
-# Este script elimina fichas de hace 10 dias de uso, y tambien limpia las bases de datos de hace 30 dias.
+# Este script elimina fichas de un lote completo y despues borra los logs del accounting y conexiones
 # Cambiar el password de tu DB
 SQLPASS="Passw@rd"
 export MYSQL_PWD=$SQLPASS
 > /tmp/expired.users.txt
 
-# extrae y despues elimina las fichas de hace 10 dias de haberlas introducido a un dispocitivo y de los perfiles 2hPausada y 12hPasauada
+# extrae y lista las fichas de la tabla fichas con el lote xxxxxx
 mysql -uroot -e "use radius; SELECT username FROM fichas WHERE batch_name = '290823J'" |sort > /tmp/expired.users.txt
+# Ahora elimina las fichas de ese lote de la base de datos
 num=0
 cat /tmp/expired.users.txt | while read users
 do
@@ -23,9 +24,9 @@ mysql -uroot -e "use radius; DELETE FROM userbillinfo WHERE username = '$USERNAM
 done
 #Copia usuarios a root
 cp /tmp/expired.users.txt /root/scripts/exp.txt
-# Parte 2 para limpiar la base de datos en radpostauth && raddact
-mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
-mysql -uroot -e "use radius; DELETE FROM radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
-mysql -uroot -e "use radius; DELETE FROM userbillinfo WHERE creationdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
-mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 30 day);"
+# Parte 2 para limpiar la base de datos en radpostauth && raddact a x tiempo
+mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
+mysql -uroot -e "use radius; DELETE FROM radacct WHERE acctstarttime <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
+mysql -uroot -e "use radius; DELETE FROM userbillinfo WHERE creationdate <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
+mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
 echo "Base de datos limpiada correctamente"
