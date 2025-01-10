519be9644af7a427fd62bbe504dcf5325a09a135
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-01-09 23:56:39 -0600

    Update cleantmpdb.sh

--- root/scripts/cleantmpdb.sh
+++ root/scripts/cleantmpdb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #set -x
 # Este script elimina las conexiones creadas en http://IP/daloradius/rep-lastconnect.php de hace cierto tiempo
+# tambien elimina las entradas en http://IP/daloradius/acct-all.php
 SQLPASS="Passw@rd"
 export MYSQL_PWD=$SQLPASS
 mysql -uroot -e "use radius; DELETE FROM radpostauth WHERE authdate <= DATE_SUB(CURDATE(), INTERVAL 60 day);"
