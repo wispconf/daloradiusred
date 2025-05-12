9be44dc026cddefa0da6226741870d203e3dbb29
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-05-12 15:19:38 -0600

    Update crearlista.sh

--- root/scripts/listar/crearlista.sh
+++ root/scripts/listar/crearlista.sh
@@ -9,7 +9,7 @@ export MYSQL_PWD=$SQLPASS
 # lista usuarios de la tabla radusergroup que tenga como grupo xxxxxxxxx
 mysql -uroot -e "use radius; SELECT username FROM radusergroup WHERE groupname = '30dCorridos'" |sort > /tmp/expired.users.txt
 # # lista usuarios de la tabla fichas que tenga como nombre de lote xxxxxxxxx
-#mysql -uroot -e "use radius; SELECT username FROM fichas WHERE batch_name = '2401237dOF'" |sort > /tmp/expired.users.txt
+#mysql -uroot -e "use radius; SELECT username FROM printme WHERE batch_name = '2401237dOF'" |sort > /tmp/expired.users.txt
 # # lista usuarios de la tabla userinfo donde el primer nombre sea xxxxxxx
 #mysql -uroot -e "use radius; SELECT username FROM userinfo WHERE userinfo.firstname like '2401212B%'" |sort > /tmp/expired.users.txt
 #Copia usuarios a /root/scripts/listar/lista.txt
