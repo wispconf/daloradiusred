be5d63a42f6ef6f6a11abb44a9b343ea0be8e131
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-06-22 19:20:46 -0700

    Update limpiaPausados.sh

--- root/scripts/limpiaPausados.sh
+++ root/scripts/limpiaPausados.sh
@@ -5,13 +5,13 @@
 SQLPASS="Passw@rd"
 Days="11"
 export MYSQL_PWD=$SQLPASS
-> /tmp/expired.users.txt
+> /tmp/pausados.txt
 
 # Fetch users who have expired 2 months ago & before, (using expired date), BE CAREFUL WHEN USING THIS
-mysql -uroot -e "use radius; SELECT username FROM usadas WHERE min <= DATE_SUB(CURDATE(), INTERVAL $Days day) and groupname like '%Pausada'" |sort > /tmp/expired.users.txt
+mysql -uroot -e "use radius; SELECT username FROM usadas WHERE min <= DATE_SUB(CURDATE(), INTERVAL $Days day) and groupname like '%Pausada'" |sort > /tmp/pausados.txt
 #mysql -uroot -e "use radius; SELECT username FROM usadas WHERE min <= DATE_SUB(CURDATE(), INTERVAL $Days day) and (groupname = '2hPausada' OR groupname = '3HrPausada' OR groupname = '12hPausada' OR groupname = '2HrPausada' OR groupname = '12HrPausada')" |sort > /tmp/expired.users.txt
 num=0
-cat /tmp/expired.users.txt | while read users
+cat /tmp/pausados.txt | while read users
 do
 num=$[$num+1]
 USERNAME=`echo $users | awk '{print $1}'`
