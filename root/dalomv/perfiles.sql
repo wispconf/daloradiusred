03e38b5ab0ed8f1bbd2b3c498e93f50011821ccf
Avi <74509380+avisonofgod@users.noreply.github.com>
2025-06-30 22:14:52 -0700

    Add files via upload

---
+++ root/dalomv/perfiles.sql
@@ -0,0 +1,164 @@
+-- MariaDB dump 10.19  Distrib 10.5.29-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: radius
+-- ------------------------------------------------------
+-- Server version	10.5.29-MariaDB-deb11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `nas`
+--
+
+DROP TABLE IF EXISTS `nas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nas` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `nasname` varchar(128) NOT NULL,
+  `shortname` varchar(32) DEFAULT NULL,
+  `type` varchar(30) DEFAULT 'other',
+  `ports` int(5) DEFAULT NULL,
+  `secret` varchar(60) NOT NULL DEFAULT 'secret',
+  `server` varchar(64) DEFAULT NULL,
+  `community` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT 'RADIUS Client',
+  PRIMARY KEY (`id`),
+  KEY `nasname` (`nasname`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nas`
+--
+
+LOCK TABLES `nas` WRITE;
+/*!40000 ALTER TABLE `nas` DISABLE KEYS */;
+INSERT INTO `nas` VALUES (1,'0.0.0.0/0','TODOS','other',0,'P@ssword','','','');
+/*!40000 ALTER TABLE `nas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `radgroupreply`
+--
+
+DROP TABLE IF EXISTS `radgroupreply`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `radgroupreply` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT '',
+  `op` char(2) NOT NULL DEFAULT '=',
+  `value` varchar(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `groupname` (`groupname`(32))
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `radgroupreply`
+--
+
+LOCK TABLES `radgroupreply` WRITE;
+/*!40000 ALTER TABLE `radgroupreply` DISABLE KEYS */;
+INSERT INTO `radgroupreply` VALUES (1,'2HrPausada','Mikrotik-Rate-Limit',':=','512K/2M 1M/3M 384K/1500K 16/12 8 256K/1000K'),(2,'2HrPausada','Acct-Interim-Interval',':=','60'),(3,'12HrPausada','Mikrotik-Rate-Limit',':=','512K/2M 1M/3M 384K/1500K 16/12 8 256K/1000K'),(4,'12HrPausada','Acct-Interim-Interval',':=','60'),(5,'7dCorridos','Mikrotik-Rate-Limit',':=','512K/2M 1M/3M 384K/1500K 16/12 8 256K/1000K'),(6,'7dCorridos','Acct-Interim-Interval',':=','60'),(7,'30dCorridos','Mikrotik-Rate-Limit',':=','512K/2M 1M/3M 384K/1500K 16/12 8 256K/1000K'),(8,'30dCorridos','Acct-Interim-Interval',':=','60'),(9,'XCorridos','Mikrotik-Rate-Limit',':=','512K/1M 1M/2M 384K/750K 16/16 8 64K/125K'),(10,'XCorridos','Acct-Interim-Interval',':=','60'),(11,'1dCorridos','Mikrotik-Rate-Limit',':=','512K/2M 1M/3M 384K/1500K 16/12 8 256K/1000K'),(12,'1dCorridos','Acct-Interim-Interval',':=','60');
+/*!40000 ALTER TABLE `radgroupreply` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `radgroupcheck`
+--
+
+DROP TABLE IF EXISTS `radgroupcheck`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `radgroupcheck` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `groupname` varchar(64) NOT NULL DEFAULT '',
+  `attribute` varchar(64) NOT NULL DEFAULT '',
+  `op` char(2) NOT NULL DEFAULT '==',
+  `value` varchar(253) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `groupname` (`groupname`(32))
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `radgroupcheck`
+--
+
+LOCK TABLES `radgroupcheck` WRITE;
+/*!40000 ALTER TABLE `radgroupcheck` DISABLE KEYS */;
+INSERT INTO `radgroupcheck` VALUES (1,'daloRADIUS-Disabled-Users','Auth-Type',':=','Reject'),(2,'daloRADIUS-Disabled-Users','Auth-Type',':=','Reject'),(3,'2HrPausada','Max-All-Session',':=','7200'),(4,'2HrPausada','Fall-Through',':=','Yes'),(5,'2HrPausada','Simultaneous-Use',':=','1'),(6,'12HrPausada','Max-All-Session',':=','43200'),(7,'12HrPausada','Fall-Through',':=','Yes'),(8,'12HrPausada','Simultaneous-Use',':=','1'),(9,'7dCorridos','Access-Period',':=','604800'),(10,'7dCorridos','Fall-Through',':=','Yes'),(11,'7dCorridos','Simultaneous-Use',':=','1'),(12,'30dCorridos','Access-Period',':=','2592000'),(13,'30dCorridos','Fall-Through',':=','Yes'),(14,'30dCorridos','Simultaneous-Use',':=','1'),(15,'XCorridos','Fall-Through',':=','Yes'),(16,'XCorridos','Simultaneous-Use',':=','1'),(17,'1dCorridos','Access-Period',':=','86400'),(18,'1dCorridos','Fall-Through',':=','Yes'),(19,'1dCorridos','Simultaneous-Use',':=','1');
+/*!40000 ALTER TABLE `radgroupcheck` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `billing_plans`
+--
+
+DROP TABLE IF EXISTS `billing_plans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `billing_plans` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `planName` varchar(128) DEFAULT NULL,
+  `planId` varchar(128) DEFAULT NULL,
+  `planType` varchar(128) DEFAULT NULL,
+  `planTimeBank` varchar(128) DEFAULT NULL,
+  `planTimeType` varchar(128) DEFAULT NULL,
+  `planTimeRefillCost` varchar(128) DEFAULT NULL,
+  `planBandwidthUp` varchar(128) DEFAULT NULL,
+  `planBandwidthDown` varchar(128) DEFAULT NULL,
+  `planTrafficTotal` varchar(128) DEFAULT NULL,
+  `planTrafficUp` varchar(128) DEFAULT NULL,
+  `planTrafficDown` varchar(128) DEFAULT NULL,
+  `planTrafficRefillCost` varchar(128) DEFAULT NULL,
+  `planRecurring` varchar(128) DEFAULT NULL,
+  `planRecurringPeriod` varchar(128) DEFAULT NULL,
+  `planRecurringBillingSchedule` varchar(128) NOT NULL DEFAULT 'Fixed',
+  `planCost` varchar(128) DEFAULT NULL,
+  `planSetupCost` varchar(128) DEFAULT NULL,
+  `planTax` varchar(128) DEFAULT NULL,
+  `planCurrency` varchar(128) DEFAULT NULL,
+  `planGroup` varchar(128) DEFAULT NULL,
+  `planActive` varchar(32) NOT NULL DEFAULT 'yes',
+  `creationdate` datetime DEFAULT '0000-00-00 00:00:00',
+  `creationby` varchar(128) DEFAULT NULL,
+  `updatedate` datetime DEFAULT '0000-00-00 00:00:00',
+  `updateby` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `planName` (`planName`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `billing_plans`
+--
+
+LOCK TABLES `billing_plans` WRITE;
+/*!40000 ALTER TABLE `billing_plans` DISABLE KEYS */;
+INSERT INTO `billing_plans` VALUES (1,'2HrPausada','','Prepaid','','Time-To-Finish','','','','','','','','No','Never','Fixed','10','8','2','MXN','','yes','2024-12-15 20:53:36','administrator',NULL,NULL),(2,'12HrPausada','','Prepaid','','Time-To-Finish','','','','','','','','No','Never','Fixed','30','24','6','MXN','','yes','2024-12-15 20:55:16','administrator',NULL,NULL),(3,'7dCorridos','','Prepaid','','Time-To-Finish','','','','','','','','No','Never','Fixed','50','40','10','MXN','','yes','2024-12-15 20:57:07','administrator',NULL,NULL),(4,'30dCorridos','','Prepaid','','Time-To-Finish','','','','','','','','No','Never','Fixed','200','160','40','MXN','','yes','2024-12-15 20:57:37','administrator',NULL,NULL),(5,'1dCorridos','','Prepaid','','Time-To-Finish','','','','','','','','No','Never','Fixed','10','8','2','MXN','','yes','2025-06-30 14:42:45','Rivera','2025-06-30 14:44:02','Rivera');
+/*!40000 ALTER TABLE `billing_plans` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-30 22:11:30
